288c1185e095cd1c5e14718001a9522eba446d35
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-12 02:57:50 +0300

    fix: update page padding for mobile view optimization

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ import ProductsList from "../components/products-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:p-24">
       <Suspense>
         <div className="flex items-center justify-between w-full">
           <h1>Products</h1>
