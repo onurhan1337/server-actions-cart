676e9332dad9c3afed656c50f029bc3ef1c514b4
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-12 02:24:25 +0300

    feat: add addToCart and removeFromCart functions to manage cart items

--- app/db/actions.ts
+++ app/db/actions.ts
@@ -16,12 +16,53 @@ export async function getAllProducts() {
 }
 
 export async function addToCart(product: CartItem) {
-  await sql`
-    INSERT INTO cart (title, price, image, quantity)
-    VALUES (${product.title}, ${product.price}, ${product.image}, ${product.quantity})
-  `;
+  try {
+    const existingProducts =
+      await sql`SELECT * FROM cart WHERE title = ${product.title}`;
 
-  revalidatePath("/cart");
+    if (existingProducts.length > 0) {
+      await sql`
+        UPDATE cart
+        SET quantity = quantity + 1
+        WHERE title = ${product.title}
+      `;
+    } else {
+      await sql`
+        INSERT INTO cart (title, price, image, quantity)
+        VALUES (${product.title}, ${product.price}, ${product.image}, ${product.quantity})
+      `;
+    }
+
+    revalidatePath("/cart");
+  } catch (error) {
+    console.error("Failed to add product to cart:", error);
+  }
+}
+
+export async function removeFromCart(product: CartItem) {
+  try {
+    const existingProducts =
+      await sql`SELECT * FROM cart WHERE title = ${product.title}`;
+
+    if (existingProducts.length > 0) {
+      if (existingProducts[0].quantity === 1) {
+        await sql`
+          DELETE FROM cart
+          WHERE title = ${product.title}
+        `;
+      } else {
+        await sql`
+          UPDATE cart
+          SET quantity = quantity - 1
+          WHERE title = ${product.title}
+        `;
+      }
+    }
+
+    revalidatePath("/cart");
+  } catch (error) {
+    console.error("Failed to remove product from cart:", error);
+  }
 }
 
 export async function getCartItems() {
