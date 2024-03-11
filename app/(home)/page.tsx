defdc2bfdc900a3f4047a947e734abec6c6392a2
DESKTOP-ABNQJBE\oguz <onurhandtr@gmail.com>
2024-03-12 02:50:30 +0300

    refac: home page layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,11 +5,11 @@ import ProductsList from "../components/products-list";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex items-center justify-between w-full">
-        <h1>Products</h1>
-        <Cart />
-      </div>
-      <Suspense fallback={<div>Products are loading...</div>}>
+      <Suspense>
+        <div className="flex items-center justify-between w-full">
+          <h1>Products</h1>
+          <Cart />
+        </div>
         <ProductsList />
       </Suspense>
     </main>
